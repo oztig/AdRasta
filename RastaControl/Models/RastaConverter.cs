05e1e1939b7683a479e9e7de98cd61f425f843c4
oztig <nj_pearson+github@icloud.com>
2025-06-24 16:37:17 +0100

    attempt to do 'Continue'

--- RastaControl/Models/RastaConverter.cs
+++ RastaControl/Models/RastaConverter.cs
@@ -6,6 +6,8 @@ using Avalonia.Controls;
 using CliWrap;
 using CliWrap.Buffered;
 using MsBox.Avalonia;
+using System.IO;
+using RastaControl.Utils;
 
 namespace RastaControl.Models;
 
@@ -35,10 +37,39 @@ public class RastaConverter
 
     public async Task ContinueConversion(string commandLocation, string SourceFile, Window _window)
     {
+        var baseDirectory = Path.GetDirectoryName(SourceFile);
+        var continueDirectory = Path.Combine(baseDirectory, ".Continue");
+        var baseCopyFileName = Path.GetFileName(SourceFile);
+        var searchPattern = baseCopyFileName.Trim() + "*.*";
+
         // Get all files in the 'source' file location (actually the destination file)
-       var messageBox =   MessageBoxManager.GetMessageBoxStandard("Continue Conversion",
+        var messageBox = MessageBoxManager.GetMessageBoxStandard("Continue Conversion",
             "Continuing Conversion of :" + SourceFile + "\n\nUsing: " + commandLocation);
-         
-         var result = await messageBox.ShowWindowDialogAsync(_window);
+
+        var result = await messageBox.ShowWindowDialogAsync(_window);
+
+        try
+        {
+            // Create Dir, if not already present
+            Directory.CreateDirectory(continueDirectory);
+
+            // Copy the images to the .Continue Directory
+            await FileUtils.CopyMatchingFilesAsync(baseDirectory, continueDirectory, searchPattern);
+            
+            // Rename the files in the .Continue Directory
+            await FileUtils.RenameMatchingFilesAsync(continueDirectory, baseCopyFileName,"*.*", "output.png");
+
+            // Copy required RastaConverter Files
+
+            // Continue
+
+            // Copy back and clear up?
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 }
---
+++ RastaControl/Utils/FileUtils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace RastaControl.Utils;
+
+public class FileUtils
+{
+    public static async Task CopyMatchingFilesAsync(string sourceDir, string destinationDir, string searchPattern)
+    {
+        await Task.Run(() =>
+        {
+            var files = Directory.GetFiles(sourceDir, searchPattern, SearchOption.TopDirectoryOnly);
+
+            foreach (var file in files)
+            {
+                var destPath = Path.Combine(destinationDir, Path.GetFileName(file));
+                File.Copy(file, destPath, overwrite: true);
+            }
+        });
+    }
+
+    public static async Task RenameMatchingFilesAsync(string sourceDir, string baseFileName, string addSearchPattern,
+        string replaceWithPattern)
+    {
+        var fullSearchPattern = baseFileName + addSearchPattern;
+        await Task.Run(() =>
+        {
+            var files = Directory.GetFiles(sourceDir, baseFileName + addSearchPattern, SearchOption.TopDirectoryOnly);
+
+            foreach (var originalFile in files)
+            {
+                if (Path.GetFileName(originalFile).Trim() != baseFileName)
+                {
+                    var newPath = originalFile.Replace(baseFileName, replaceWithPattern);
+                    File.Move(originalFile, newPath, overwrite: true);
+                    File.SetCreationTime(newPath,DateTime.Now);
+                    File.SetLastWriteTime(newPath, DateTime.Now);
+                    File.SetLastAccessTime(newPath, DateTime.Now);
+                }
+            }
+        });
+    }
+}
